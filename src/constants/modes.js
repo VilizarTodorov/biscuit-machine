72720e291c07b35570c162754f28ae5ca1dbf880
VilizarTodorov <todorovvilizar@gmail.com>
2022-01-05 16:38:24 +0200

    add constants

---
+++ src/constants/modes.js
@@ -0,0 +1,10 @@
+export const MACHINE_MODES = {
+    OFF: 'OFF',
+    ON: 'ON',
+    PAUSED: 'PAUSED ',
+};
+
+export const OVEN_HEATING_ELEMENT_MODE = {
+    ON: 'ON',
+    OFF: 'OFF',
+};
---
+++ src/constants/temepratures.js
@@ -0,0 +1,8 @@
+// minimum required temperature to bake a cookie
+export const MIN_TEMPERATURE = 220;
+
+// maximum temperature a cookie can be baked at
+export const MAX_TEMPERATURE = 240;
+
+// the amount with which the oven increases/decreases it's temperature 
+export const TEMPERATURE_VARIATION = 10;
